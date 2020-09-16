c1cc70067c53fdf50a73d7e46332e1e0b54cd3ff
Crea7dosSantos <cr26yusaku0720@icloud.com>
2020-09-16 19:55:15 +0900

    close #3

---
+++ pages/blogs/[id].js
@@ -0,0 +1,49 @@
+import fetch from 'isomorphic-unfetch';
+
+const Blog = ({ blog }) => {
+    return (
+        <div>
+            <h1>{blog.title}</h1>
+            <div>
+                <p>記事作成日時: {blog.created_at}</p>
+                <p>{blog.body}</p>
+            </div>
+        </div>
+    )
+}
+
+export const getStaticPaths = async () => {
+    const key = {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+    }
+    const res = await fetch(
+        'https://next-jamstack.microcms.io/api/v1/blogs',
+        key
+    )
+    const repos = await res.json()
+
+    const paths = repos.contents.map(repo => `/blogs/${repo.id}`)
+    return { paths, fallback: false };
+}
+
+export const getStaticProps = async context => {
+    const id = context.params.id
+
+    const key = {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+    }
+    const res = await fetch(
+        `https://next-jamstack.microcms.io/api/v1/blogs/${id}`,
+        key
+    )
+    const blog = await res.json();
+
+    return {
+        props: {
+            blog: blog
+        }
+    }
+}
+
+export default Blog
+
